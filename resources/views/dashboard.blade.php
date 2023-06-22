3eb663d0226f06d748858f1e1ee34d35d46357a1
Leon14789 <leoortizinho23@gmail.com>
2023-06-22 13:37:53 -0300

    create base main layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,18 @@
-<h1>teste</h1>
+
+
+<!-- Este cara esta extendendo da main ou seja tudo que tem lá tera aqui  -->
+@extends('layouts.main')
+
+<!--  para o titulo vir tinamicamente definimos a section 
+    chamamos o title seguido de virgula dps 'o titulo' -->
+
+@section('title', 'Dashboard')
+
+<!--  e para reidenizar o conteudo dinamicamente chamamos 
+    a section novamente e dps content -->
+@section('content')
+
+
+<h1>Aqui esta o conteudo do Dashboard</h1>
+
+@endsection
