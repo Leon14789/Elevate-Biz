f5bc3ed6f33b35b9b6331549a8a16e2c3593c970
Leon14789 <leoortizinho23@gmail.com>
2023-06-28 18:45:50 -0300

    style header and menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,39 @@
-<head> 
-       
-            <i class="icofont-duotone icofont-list-thin icofont-3x"></i>
+<header class="header"> 
     
-
-    
+    <div class="logo mr-3 ">
             <h3>Elevate Biz</h3>
+    </div>
        
-   
-        <!-- <div class="user-menu">
 
-        </div> -->
+    <div class="menu">
+        <i class="icofont-duotone icofont-list-thin icofont-3x"></i>
+    </div>
 
-</head>
+    <div class="spacer"></div>
+
+    <div class="menu-user">
+        <div class="menu-user-button">
+            <span class="ml-2">Nome Users</span>
+            <i class="icofont-duotone icofont-click icofont-1x mx-2"></i>
+           
+        </div>
+        <div class="menu-user-content">
+            <ul class="nav-list">
+                <li class="nav-item">
+                    <a href="/login">Sair</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/login">Sair</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/login">Sair</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/login">Sair</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</header>
+
