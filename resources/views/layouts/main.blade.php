ddd357f31c364536d1f18cc77a5b33924b2312fa
Leon14789 <leoortizinho23@gmail.com>
2023-07-12 20:24:44 -0300

    inset values root css in layouts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/icofont.css">
     <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/root.css">
     <link rel="stylesheet" href="/assets/css/header.css">
     <link rel="stylesheet" href="/assets/css/mainContent.css">
     <link rel="stylesheet" href="/assets/css/footer.css">
